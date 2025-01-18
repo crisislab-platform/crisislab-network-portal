4614e1c4df6fb5ef677870353e6353db2e0c6023
propulsion180 <wmmperera@tuta.io>
2025-01-18 18:20:46 +1300

    Got the buttons working. Header bar is complete. Can add pages easily.

--- frontend/Header.tsx
+++ frontend/Header.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 
 export default function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
   const goToTable = () => { navigate('/table')};
   const goToMap = () => { navigate('/map')}
   return (
-    <div>
-      <button onClick={goToTable}>Table</button>
-      <button onClick={goToMap}>Map</button>
-    </div>
+    <div className='header-div'>
+      <div className='title-format'>
+        <a onClick={goToMap}><h1>CrisisLab Lora Network Portal</h1></a>
+      </div>
+      <div className='center'>
+        <a
+          className={`nav-button ${location.pathname === '/table' ? 'active' : ''}`}
+          onClick={goToTable}
+        >
+          Table
+        </a>
+        <a
+          className={`nav-button ${location.pathname === '/map' ? 'active' : ''}`}
+          onClick={goToMap}
+        >
+          Map
+        </a>
+      </div>
+     </div> 
   )
 }

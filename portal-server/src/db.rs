04f41b4b808bcceefb0866efe88db122c52e2f14
propulsion180 <wmmperera@tuta.io>
2025-01-22 10:59:53 +1300

    wrote add_user and validate_password function

--- portal-server/src/db.rs
+++ portal-server/src/db.rs
@@ -1,4 +1,5 @@
-use rusqlite::{params, Connection, Result};
+use bcrypt::{hash, verify, DEFAULT_COST};
+use rusqlite::{params, Connection, OptionalExtension, Result};
 
 pub fn initialize_database(db_path: &str) -> Result<Connection> {
     let conn = Connection::open(db_path)?;
@@ -9,7 +10,8 @@ pub fn initialize_database(db_path: &str) -> Result<Connection> {
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT UNIQUE NOT NULL,
                 password TEXT NOT NULL,
-                is_admin BOOLEAN NOT NULL
+                is_admin BOOLEAN NOT NULL,
+                token TEXT DEFAULT ''
             );
             ",
             [],
@@ -29,4 +31,38 @@ pub fn initialize_database(db_path: &str) -> Result<Connection> {
         }
     }
     Ok(conn)
+}
+
+pub fn verify_password(conn: &Connection, username: &str, password: &str) -> Result<bool> {
+    let mut stmt = conn.prepare("SELECT password FROM users WHERE username = ?1")?;
+    let hashed_password: Option<String> = stmt
+        .query_row(params![username], |row| row.get(0))
+        .optional()?;
+
+    if let Some(hashed_password) = hashed_password {
+        let is_valid = verify(password, &hashed_password).unwrap_or(false);
+        Ok(is_valid)
+    } else {
+        Ok(false)
+    }
+}
+
+pub fn add_user(
+    conn: &Connection,
+    operator_uname: &str,
+    uname: &str,
+    pword: &str,
+    admin: &bool,
+) -> Result<bool> {
+    let mut can_add_stmt = conn.prepare("SELECT is_admin FROM users WHERE username = ?;");
+    let can_add: bool = can_add_stmt.query_row(params![operator_uname], |row| row.get(0))?;
+    if !can_add {
+        Ok(false)
+    }
+
+    let mut add_user_stmt = conn.execute(
+        "INSERT INTO users (username, password, is_admin) VALUES (?, ?, ?);",
+        params![uname, hash(pword, DEFAULT_COST), admin],
+    )?;
+    Ok(true)
 }

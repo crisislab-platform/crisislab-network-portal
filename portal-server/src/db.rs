691b1b296d94b81bcb48c3b31b9c16f03886ae39
propulsion180 <wmmperera@tuta.io>
2025-01-23 20:55:07 +1300

    finished all the database methods that I think we need. Started working on the routes that may be needed by the webapp.

--- portal-server/src/db.rs
+++ portal-server/src/db.rs
@@ -1,4 +1,5 @@
 use bcrypt::{hash, verify, DEFAULT_COST};
+use rusqlite::Error as RusqliteError;
 use rusqlite::{params, Connection, OptionalExtension, Result};
 
 pub fn initialize_database(db_path: &str) -> Result<Connection> {
@@ -47,22 +48,96 @@ pub fn verify_password(conn: &Connection, username: &str, password: &str) -> Res
     }
 }
 
-pub fn add_user(
+pub fn new_user(
     conn: &Connection,
     operator_uname: &str,
     uname: &str,
     pword: &str,
     admin: &bool,
 ) -> Result<bool> {
-    let mut can_add_stmt = conn.prepare("SELECT is_admin FROM users WHERE username = ?;");
+    let mut can_add_stmt = conn.prepare("SELECT is_admin FROM users WHERE username = ?;")?;
     let can_add: bool = can_add_stmt.query_row(params![operator_uname], |row| row.get(0))?;
     if !can_add {
         Ok(false)
+    } else {
+        let mut add_user_stmt = conn.execute(
+            "INSERT INTO users (username, password, is_admin) VALUES (?, ?, ?);",
+            params![uname, hash(pword, DEFAULT_COST).ok(), admin],
+        )?;
+        Ok(true)
+    }
+}
+
+pub fn set_token(conn: &Connection, uname: &str, token: &str) -> Result<()> {
+    conn.execute(
+        "UPDATE users SET token = ?1 WHERE username = ?2;",
+        params![token, uname],
+    )?;
+    Ok(())
+}
+
+pub fn get_token(conn: &Connection, uname: &str) -> Result<Option<String>> {
+    let mut stmt = conn.prepare("SELECT token FROM users WHERE username = ?1")?;
+
+    let token: Option<String> = stmt
+        .query_row(params![uname], |row| row.get(0))
+        .optional()?;
+
+    Ok(token)
+}
+
+pub fn delete_user(conn: &Connection, operator_uname: &str, uname: &str) -> Result<bool> {
+    let mut stmt = conn.prepare("SELECT is_admin FROM users WHERE username = ?;")?;
+    if !stmt.query_row(params![operator_uname], |row| row.get(0))? {
+        Ok(false)
+    } else {
+        conn.execute("DELETE FROM users WHERE username = ?1", params![uname])?;
+        Ok(true)
+    }
+}
+
+pub fn update_password(
+    conn: &Connection,
+    operator_uname: &str,
+    uname: &str,
+    old_password: &str,
+    new_password: &str,
+) -> Result<bool> {
+    let mut auth: bool = true;
+    if operator_uname != uname {
+        auth = false;
+    }
+    if !conn
+        .prepare("SELECT is_admin FROM users WHERE username = ?1")?
+        .query_row(params![operator_uname], |row| row.get(0))?
+    {
+        auth = false;
     }
 
-    let mut add_user_stmt = conn.execute(
-        "INSERT INTO users (username, password, is_admin) VALUES (?, ?, ?);",
-        params![uname, hash(pword, DEFAULT_COST), admin],
-    )?;
-    Ok(true)
+    let oldP = hash(old_password, DEFAULT_COST).ok();
+    let newP = hash(new_password, DEFAULT_COST).ok();
+
+    let stored_password: String = conn
+        .prepare("SELECT password FROM users WHERE username = ?1")?
+        .query_row(params![uname], |row| row.get(0))?;
+
+    match verify(old_password, &stored_password) {
+        Ok(is_valid) => {
+            if !is_valid {
+                auth = false;
+            }
+        }
+
+        Err(_) => return Ok(false),
+    }
+
+    if auth {
+        conn.execute(
+            "UPDATE users SET password = ?1 WHERE username = ?2;",
+            params![newP, uname],
+        )?;
+        Ok(true)
+    } else {
+        Ok(false)
+    }
 }
--- portal-server/src/main.rs
+++ portal-server/src/main.rs
@@ -1,10 +1,151 @@
 #[macro_use]
 extern crate rocket;
+use bcrypt::{hash, verify, DEFAULT_COST};
+use chrono::Utc;
+use db::{delete_user, initialize_database, new_user, verify_password};
+use jsonwebtoken::{encode, EncodingKey, Header};
 use rocket::fs::NamedFile;
 use rocket::fs::{relative, FileServer};
+use rocket::response::status;
+use rocket::serde::{json::Json, Deserialize, Serialize};
+use rocket::State;
 use std::path::Path;
+use std::sync::Mutex;
 
 mod db;
+
+/// JWT Claims structure
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String, // Subject (username)
+    exp: usize,  // Expiration time (as a UNIX timestamp)
+}
+
+/// Login payload
+#[derive(Deserialize)]
+struct LoginData {
+    username: String,
+    password: String,
+}
+
+/// Login response
+#[derive(Serialize)]
+struct LoginResponse {
+    token: String,
+}
+
+#[derive(Deserialize)]
+struct NewUserdata {
+    creator: String,
+    username: String,
+    password: String,
+    isAdmin: bool,
+}
+
+#[derive(Serialize)]
+struct NewUserResponse {
+    username: String,
+}
+
+#[derive(Deserialize)]
+struct RemoveUserData {
+    remover: String,
+    username: String,
+}
+
+#[derive(Serialize)]
+struct RemoveUserResponse {
+    removed_user: String,
+}
+
+/// Secret key for JWT
+const SECRET_KEY: &[u8] = b"yowzabazinga";
+
+/// Login endpoint
+#[post("/login", data = "<login_data>")]
+async fn login(
+    login_data: Json<LoginData>,
+    conn: &State<Mutex<rusqlite::Connection>>,
+) -> Result<Json<LoginResponse>, status::Unauthorized<String>> {
+    // Extract login data
+    let login_data = login_data.into_inner();
+    let conn = conn.lock().unwrap();
+
+    let verify_result = verify_password(&conn, login_data.username, login_data.password)?;
+
+    if verify_result {
+        // Create JWT claims
+        let claims = Claims {
+            sub: login_data.username,
+            exp: chrono::Utc::now().timestamp() as usize + 3600,
+        };
+
+        // Generate the token
+        let token = encode(
+            &Header::default(),
+            &claims,
+            &EncodingKey::from_secret(SECRET_KEY),
+        )
+        .map_err(|_| status::Unauthorized(Some("Token generation failed".into())))?;
+
+        // Return the token as JSON
+        Ok(Json(LoginResponse { token }))
+    } else {
+        // Handle invalid credentials
+        Err(status::Unauthorized(
+            "Invalid username or password".to_string(),
+        ))
+    }
+}
+
+/// Add User endpoint
+#[post("/add_user", data = "<user_data>")]
+async fn add_user(
+    user_data: Json<NewUserdata>,
+    conn: &State<Mutex<rusqlite::Connection>>,
+) -> Result<Json<NewUserResponse>, status::Unauthorized<String>> {
+    // Extract login data
+    let add_user_data = user_data.into_inner();
+    let conn = conn.lock().unwrap();
+
+    let passed = new_user(
+        &conn,
+        add_user_data.creator,
+        add_user_data.username,
+        add_user_data.password,
+        add_user_data.isAdmin,
+    )
+    .map_err(|_| status::Unauthorized(Some("Failed to write user to database".into())))?;
+    let username: String = add_user_data.username;
+    if passed {
+        Ok(Json(NewUserResponse { username }))
+    } else {
+        Err(status::Unauthorized(
+            "invalid input data or incorrect permissions".to_string(),
+        ))
+    }
+}
+
+#[delete("/remove_user", data = "<user_data>")]
+async fn remove_user(
+    user_data: Json<RemoveUserData>,
+    conn: &State<Mutex<rusqlite::Connection>>,
+) -> Result<Json<RemoveUserResponse>, status::Unauthorized<String>> {
+    let remove_user_data = user_data.into_inner();
+    let conn = conn.lock().unwrap();
+
+    let passed: bool = delete_user(&conn, remove_user_data.remover, remove_user_data.username)?;
+
+    let removed_user: String = remove_user_data.username;
+
+    if passed {
+        Ok(Json(RemoveUserResponse { removed_user }))
+    } else {
+        Err(status::Unauthorized(
+            "invalid permissons or user doesn't exist".to_string(),
+        ))
+    }
+}
 
 #[get("/")]
 async fn index() -> Option<NamedFile> {
@@ -13,7 +154,9 @@ async fn index() -> Option<NamedFile> {
 
 #[launch]
 fn rocket() -> _ {
+    let conn = initialize_database("users.db").expect("Failed to initialize database");
     rocket::build()
-        .mount("/", routes![index]) // Route for `/`
+        .manage(Mutex::new(conn))
+        .mount("/", routes![index, login,]) // Route for `/`
         .mount("/public", FileServer::from(relative!("public"))) // Serve other static files
 }

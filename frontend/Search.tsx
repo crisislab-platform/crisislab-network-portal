005ffb609226c3301c5767d1311b3ecab2ff454a
propulsion180 <wmmperera@tuta.io>
2025-02-12 19:48:58 +1300

    The table works with the search parameters

--- frontend/Search.tsx
+++ frontend/Search.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 
 export default function Search({ nodes }) {
+  const [num, setNum] = useState<string>("");
   const [username, setUsername] = useState<string>("");
   const [devShortName, setDevShortName] = useState<string>("");
   const [devLongName, setDevLongName] = useState<string>("");
+  const nodeList = Array.from(nodes.values());
   return (
     <div>
       <h3>There are currently {nodes.size} nodes to search</h3>
-      <form className="form-div-h">
+      <div className="form-div-h">
+        <div className="form-section-div-h">
+          <label className='form-label' htmlFor='num'>Num: </label>
+          <input className="form-input" id="num" type="text" value={num} onChange={(e) => setNum(e.target.value)} />
+        </div>
         <div className="form-section-div-h">
           <label className='form-label' htmlFor='username'>Username: </label>
           <input className="form-input" id="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
@@ -20,8 +26,60 @@ export default function Search({ nodes }) {
           <label className='form-label' htmlFor='devLongName'>LName: </label>
           <input className="form-input" id="devLongName" type="text" value={devLongName} onChange={(e) => setDevLongName(e.target.value)} />
         </div>
-        <button className='form-submit-button-search' type='submit'>Submit</button>
-      </form>
+      </div>
+      <table style={{ borderCollapse: "collapse", width: "100%" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>Num</th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Username
+            </th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Long Name
+            </th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Short Name
+            </th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>SNR</th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Last Heard
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+        {nodeList
+          .filter(
+            (node) =>
+              node.num === Number(num) ||
+              node.user.id == username ||
+              node.user.short_name === devShortName ||
+              node.user.long_name === devLongName
+          )
+          .map((node) => (
+            <tr key={node.num}>
+              <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                {node.num}
+              </td>
+              <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                {node.user.id}
+              </td>
+              <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                {node.user.long_name}
+              </td>
+              <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                {node.user.short_name}
+              </td>
+              <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                {node.snr}
+              </td>
+              <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                {new Date(node.last_heard * 1000).toLocaleString()}
+              </td>
+            </tr>
+          ))}
+          
+        </tbody>
+      </table>
     </div>
   );
 }
